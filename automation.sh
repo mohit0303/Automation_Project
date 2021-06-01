1033d8c744a60b23383896ff2f25fdbbb3dfb833
root <root@ip-172-31-21-240.ec2.internal>
2021-06-01 20:02:52 +0000

    Task 2: Apache server installation and zipping logs file and coping to s3 bucket

--- automation.sh
+++ automation.sh
@@ -2,36 +2,50 @@ myname="mohit"
 s3_bucket="upgrad-mohit"
 
 #Update the packages
-echo "Updating the packages.."
+echo "***********************Updating the packages************************"
 sudo apt update -y
 
-#To check if apache is already installed or not
+############################# To check if apache is already installed or not #############################
 dpkg -s apache2
 
-#install apache if not already installed
 if [ $? -eq 0 ]
 then
-	echo "Apache2 server is installed."
+        echo "*****************************Apache2 Server is Installed -> CHECKED*******************************"
 else
-	echo "Installing Apache2 Server..."
-	sudo apt-get install apache2 -y
+        echo "***********************Installing Apache2 Server***************************"
+        sudo apt-get install apache2 -y
 fi
-#to check if APACHE is RUNNING if installed
-#service apache2 status
-#if [ $? -eq 0 ]
-#then
-#	echo "Apache is running!"
-#else
-#	echo "Running Apache.."
-#	service apache2 restart
-#fi
-#to check of APACHE is ENABLED
+
+####################################### To check if APACHE is RUNNING or not ###############################
+service apache2 status
+
+if [ $? -eq 0 ]
+then
+        echo "**********************************Apache is running -> CHECKED******************************"
+else
+        echo "**********************************WAIT Running Apache*************************"
+        service apache2 start
+fi
+
+################################################ To check of APACHE is ENABLED ################################
+systemctl list-unit-files |  grep enabled | grep apache2
+
+if [ $? -eq 0 ]
+then
+        echo "*******************************Apache is ENABLED -> CHECKED********************************"
+else
+        echo "*******************************Running Apache****************************"
+        sudo systemctl enable apache2
+fi
+##############################################################################################################
 
 #Creatng tar of logs file in temp folder and pushing it in S3
-
+echo "********************************Creating Timestamp and making tar file********************"
 timestamp=$( (date '+%d%m%Y-%H%M%S') )
-echo "$timestamp"
 tar -zcvf /tmp/$myname-httpd-logs-$timestamp.tar  --exclude=other_vhosts_access.log   /var/log/apache2/*.log
+#taking tar size
+tar_size=$( (du -k /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -f1) )
+echo "**************************Pushing Tar to upgrad-Mohit S3 Bucket**************************"
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
